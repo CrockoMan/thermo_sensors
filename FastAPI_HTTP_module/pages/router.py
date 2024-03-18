d40a2cf5c255f45a26af911e5078603e9206161e
Константин Гурашкин <crocko@mail.ru>
2024-03-18 17:44:13 +0300

    Added save settings view, serializer

--- FastAPI_HTTP_module/pages/router.py
+++ FastAPI_HTTP_module/pages/router.py
@@ -1,6 +1,11 @@
 from fastapi import APIRouter, Depends, Request
 from fastapi.templating import Jinja2Templates
+from fastui import FastUI, AnyComponent, prebuilt_html, components as c
+from fastui.components.display import DisplayMode, DisplayLookup
+from fastui.events import GoToEvent, BackEvent
 
+from main import app
+from sensors.dao import SensorDAO
 from sensors.router import get_sensors
 
 router_pages = APIRouter(prefix="", tags=["frontend"])
@@ -14,3 +19,26 @@ async def get_sensors(request: Request,
                                       context={'request': request,
                                                'sensors': sensors}
                                       )
+
+
+@app.get("/api/view/", response_model=FastUI, response_model_exclude_none=True)
+async def users_table() -> list[AnyComponent]:
+    """
+    Show a table of four users, `/api` is the endpoint the frontend will connect to
+    when a user visits `/` to fetch components to render.
+    """
+    return [
+        c.Page(
+            components=[
+                c.Heading(text='Users', level=2),  # renders `<h2>Users</h2>`
+                c.Table(
+                    data=await SensorDAO.find_all(),
+                    columns=[
+                        DisplayLookup(field='name', ),
+                        # DisplayLookup(field='name', on_click=GoToEvent(url='/user/{id}/')),
+                        # DisplayLookup(field='dob', mode=DisplayMode.date),
+                    ],
+                ),
+            ]
+        ),
+    ]
